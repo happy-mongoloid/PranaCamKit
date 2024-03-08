209a39db63280365b1b223bf0b5d50462a2fec2c
Nezhdanov Demian <nzhd.demian@gmail.com>
2024-03-08 00:09:10 -0300

    init

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+    import PackageDescription
+
+    let package = Package(
+        name: "PranaCamKit",
+        platforms: [
+          .macOS(.v12), .iOS(.v11)
+        ],
+        products: [
+            // Products define the executables and libraries a package produces, and make them visible to other packages.
+            .library(
+                name: "PranaCamKit",
+                targets: ["PranaCamKit"]),
+        ],
+        dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            // .package(url: /* package url */, from: "1.0.0"),
+        ],
+        targets: [
+
+            .binaryTarget(
+                name: "PranaCamKit",
+                path: "./Sources/PranaCamKit.xcframework")
+        ]
+    )
---
+++ Sources/PranaCamKit.xcframework/ios-arm64/PranaCamKit.framework/Headers/PranaCamKit.h
@@ -0,0 +1,18 @@
+//
+//  PranaCamKit.h
+//  PranaCamKit
+//
+//  Created by Demian Nezhdanov on 07/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PranaCamKit.
+FOUNDATION_EXPORT double PranaCamKitVersionNumber;
+
+//! Project version string for PranaCamKit.
+FOUNDATION_EXPORT const unsigned char PranaCamKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PranaCamKit/PublicHeader.h>
+
+
